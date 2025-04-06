8e116903b045c48cefd0d53f2cd584d4370727f4
ntkskwk <natsukawa247@outlook.com>
2025-04-06 10:22:19 +0800

    fix(MediaEncode): Update output filename format in batch script

--- MediaEncode/视频压制.bat
+++ MediaEncode/视频压制.bat
@@ -7,7 +7,7 @@ chcp 65001
 rem 遍历输入目录下的所有mkv文件
 for %%a in (*.MP4) do (
     set filename=%%~na
-    ffmpeg -i "%%a" -c:v hevc_qsv -rc vbr_hq -cq 26 -preset slow -profile:v main "!filename!RERIP.mp4"
+    ffmpeg -i "%%a" -c:v hevc_qsv -rc vbr_hq -cq 26 -preset slow -profile:v main "!filename![natsuu][RERIP].mp4"
 )
 
 # :charenc=UTF-8
